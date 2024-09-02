cf8323e6354bf3dff8e90a69862b1b45a53b183f
from11 <joshua.tuinder@gmail.com>
2024-09-02 11:40:31 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => event.preventDefault());
+const input = document.querySelector('#cost-input');
+input.addEventListener('change', (event) => {
+  updateOutputs(event.srcElement.value);
+});
+
+const updateOutputs = (inputValue) => {
+  document.querySelector('#output-10').textContent = `$${(Math.round(inputValue * 1.1 * 100) / 100).toFixed(2)}`;
+  document.querySelector('#output-20').textContent = `$${(Math.round(inputValue * 1.2 * 100) / 100).toFixed(2)}`;
+  document.querySelector('#output-25').textContent = `$${(Math.round(inputValue * 1.25 * 100) / 100).toFixed(2)}`;
+  document.querySelector('#output-45').textContent = `$${(Math.round(inputValue * 1.45 * 100) / 100).toFixed(2)}`;
+};
