2de30f6b3936aec0f137b1851efd27aa8c7c3977
from11 <joshua.tuinder@gmail.com>
2024-10-01 07:13:49 -0600

    added 30%

--- script.js
+++ script.js
@@ -9,5 +9,6 @@ const updateOutputs = (inputValue) => {
   document.querySelector('#output-10').textContent = `$${(Math.round(inputValue * 1.1 * 100) / 100).toFixed(2)}`;
   document.querySelector('#output-20').textContent = `$${(Math.round(inputValue * 1.2 * 100) / 100).toFixed(2)}`;
   document.querySelector('#output-25').textContent = `$${(Math.round(inputValue * 1.25 * 100) / 100).toFixed(2)}`;
+  document.querySelector('#output-30').textContent = `$${(Math.round(inputValue * 1.3 * 100) / 100).toFixed(2)}`;
   document.querySelector('#output-45').textContent = `$${(Math.round(inputValue * 1.45 * 100) / 100).toFixed(2)}`;
 };
